c075d11d942df01226f01466b458750e4345a897
aditya <111045036+aditya4737@users.noreply.github.com>
2023-11-13 02:10:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+let ClickAudio = new Audio("click.wav");
+let winnerAudio = new Audio("success-1-6297.mp3");
 let ClickCount=1;
+
+
 for (var i = 0; i < 9; i++) {
     document.querySelectorAll(".box")[i].addEventListener("click", function () {
         if(ClickCount%2!==0){
             this.querySelector(".boxtext").innerHTML = "X";
+            ClickAudio.play();
         }
         else{
             this.querySelector(".boxtext").innerHTML = "O";
+            ClickAudio.play();
         }
         ClickCount++;
     });
 }
+
